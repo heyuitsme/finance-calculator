a8470ce5cfb1c17775d599eadd6905f1571fc2d9
Wallace L <31195979+heyuitsme@users.noreply.github.com>
2017-11-13 23:57:22 -0500

    mainCommit

--- main.js
+++ main.js
@@ -1,45 +1,89 @@
 /* Finance Calculator main JS */
 
-function calculate() {
+function invert() {
+  document.getElementById('loanText').innerHTML = 'Monthly Payment:';
+};
 
-  var loanAmt = document.getElementById('loanAmount').value;
+function uninvert() {
+  document.getElementById('loanText').innerHTML = 'Loan Amount:';
+};
+
+function firstInput() {
+  var selection = document.getElementById('invertChoice').value;
+
+  if(selection == 'totCalc') {
+    invert();
+  } else if(selection == 'monthCalc') {
+    uninvert();
+  }
+};
+
+function findAmount() {
+  var selection = document.getElementById('invertChoice').value;
+
+  if(selection == 'monthCalc') {
+    calculate();
+  } else if(selection == 'totCalc') {
+    invertCalc();
+  }
+};
+
+function calculate() {
+  document.getElementById('loanText').value = 'loan';
+  var loanAmt = document.getElementById('amount').value;
   var intRate = (document.getElementById('interestRate').value)/100;
   var finTerm = document.getElementById('financeTerm').value;
   var tax = (document.getElementById('salesTax').value)/100;
   var principal = loanAmt*(1+tax);
 
   var monthlyPay = ((intRate/12)*principal)/(1-((1+intRate/12)**(-1*(finTerm))));
-  console.log(monthlyPay);
-  disOutput();
-
-
-  function interestCalc() {
-    var totalInterestPaid = ((monthlyPay * finTerm) - loanAmt);
-    return totalInterestPaid;
-  }
-
-  function totalCalc() {
-    var totalObligation = (monthlyPay * finTerm);
-    return totalObligation;
-  }
-
-  function monthlyMess() {
-    'Monthly payment = $' + monthlyPay.toFixed(2);
-  }
-  function intMess() {
-    'Total interest paid = $' + totalInterestPaid.toFixed(2);
-  }
-  function totMess() {
-    'Total obligation = $' + totalObligation.toFixed(2);
-  }
 
   function disOutput() {
+    function monthlyMess() {
+      return 'Monthly payment = $' + monthlyPay.toFixed(2);
+    }
+    function intMess() {
+      var totalInterestPaid = ((monthlyPay * finTerm) - loanAmt);
+      return 'Total interest paid = $' + totalInterestPaid.toFixed(2);
+    }
+    function totMess() {
+      var totalObligation = (monthlyPay * finTerm);
+      return 'Total obligation = $' + totalObligation.toFixed(2);
+    }
     document.getElementById('monthlyPayment').innerHTML = monthlyMess();
     document.getElementById('totalInterest').innerHTML = intMess();
     document.getElementById('totalPaid').innerHTML = totMess();
   }
 
-  console.log('shit is working');
-  console.log(loanAmt+intRate);
-  console.log(principal.toFixed(2));
+  disOutput();
+
+};
+
+function invertCalc() {
+  var monthlyPay = document.getElementById('amount').value;
+  var intRate = (document.getElementById('interestRate').value)/100;
+  var finTerm = document.getElementById('financeTerm').value;
+  var tax = (document.getElementById('salesTax').value)/100;
+
+  var loanAmt = ((monthlyPay * (1-(1+intRate/12)**(-1*finTerm)))/(intRate/12))/(1+tax);
+  console.log(loanAmt);
+
+  function disOutput() {
+    function loanMess() {
+      return 'Loan amount = $' + loanAmt.toFixed(2);
+    }
+    function intMess() {
+      var totalInterestPaid = ((monthlyPay * finTerm) - loanAmt);
+      return 'Total interest paid = $' + totalInterestPaid.toFixed(2);
+    }
+    function totMess() {
+      var totalObligation = (monthlyPay * finTerm);
+      return 'Total obligation = $' + totalObligation.toFixed(2);
+    }
+    document.getElementById('monthlyPayment').innerHTML = loanMess();
+    document.getElementById('totalInterest').innerHTML = intMess();
+    document.getElementById('totalPaid').innerHTML = totMess();
+  }
+
+  disOutput();
 };
